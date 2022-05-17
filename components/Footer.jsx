cbb8f237e7cd40f9be7e509afd07c30d9c8a605b
maximilian-grh <maximilian@griehsler.info>
2022-05-17 11:26:16 +0200

    Version 0.0.3

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div className="text-center justify-center items-center mx-auto flex flex-col mb-10">
+      <div className="flex gap-4">
+        <p>© Copyright 2022 COMPANY</p>
+        <p>Alle Rechte vorbehalten</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
 import type { NextPage } from "next";
+import "tailwindcss/tailwind.css";
+
 import Navbar from "../components/Navbar";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
-import "tailwindcss/tailwind.css";
 import Checkout from "./checkout";
+import Feature from "../components/Feature";
+import ProductFeature from "../components/ProductFeature";
+import InfoBanner from "../components/InfoBanner";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout>
+        <InfoBanner />
         <Navbar />
         <Hero />
+        <Feature />
+        <ProductFeature />
+        <Footer />
       </Layout>
     </>
   );
