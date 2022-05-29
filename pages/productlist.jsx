2f2fd0a5a8216262497608125813f1ff00495f45
maximilian-grh <maximilian@griehsler.info>
2022-05-29 17:36:50 +0200

    Version 0.2.0

--- pages/productlist.jsx
+++ pages/productlist.jsx
@@ -3,33 +3,40 @@ import initStripe from "stripe";
 
 const Pricing = ({ objects }) => {
   return (
-    <div className="bg-white">
+    <div className="bg-white h-screen">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+        <h2 className="text-2xl font-semibold tracking-tight text-gray-900">
           Unsere Produkte
         </h2>
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <div className="w-full max-w-3xl mx-auto py-16 flex justify-around">
+        <a href="/" className="">
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {objects.map((object) => (
-              <div
-                key={object.id}
-                className="hover:bg-gray-200 cursor-pointer w-80 h-40 rounded shadow px-6 py-4 group relative"
-              >
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <div key={object.id} className="group relative cursor-pointer">
+                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-xl overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                   <img
                     src={object.images[0]}
                     alt={object.id}
-                    class="h-auto w-auto"
+                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                   />
                 </div>
                 <div className="mt-4 flex justify-between">
-                  <h2 className="text-xl">{object.name}</h2>
-                  <p className="text-gray-500">${object.price / 100}</p>
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href={object.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {object.name}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">{object.color}</p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">
+                    {object.price / 100}€
+                  </p>
                 </div>
               </div>
             ))}
           </div>
-        </div>
+        </a>
       </div>
     </div>
   );
